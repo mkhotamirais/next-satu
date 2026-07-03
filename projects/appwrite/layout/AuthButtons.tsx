5c12dc6bf981a861637a266aa800121a8512abbe
mkhotamirais <mkhotamirais@gmail.com>
2026-07-03 10:58:21 +0700

    ssg

--- projects/appwrite/layout/AuthButtons.tsx
+++ projects/appwrite/layout/AuthButtons.tsx
@@ -1,72 +1,8 @@
-"use client";
+import { getLoggedInUser } from "@/lib/server/appwrite.query";
+import AuthButtonsClient from "./AuthButtonsClient";
 
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { signOut } from "@/actions/appwrite/auth";
-import { User } from "@/lib/types/appwrite";
+export default async function AuthButtons() {
+  const user = await getLoggedInUser();
 
-export default function AuthButtons({ user }: { user: User | null }) {
-  if (user) {
-    return (
-      <div>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" className="grayscale" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuGroup>
-              <DropdownMenuLabel>Hi {user.name}</DropdownMenuLabel>
-              <DropdownMenuItem asChild>
-                <Link href="/appwrite/account" className="w-full">
-                  Profile
-                </Link>
-              </DropdownMenuItem>
-              {user.role === "admin" && (
-                <DropdownMenuItem asChild>
-                  <Link href="/appwrite/dashboard" className="w-full">
-                    Dashboard
-                  </Link>
-                </DropdownMenuItem>
-              )}
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem
-                variant="destructive"
-                onClick={async () => {
-                  await signOut();
-                }}
-              >
-                Sign out
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
-    );
-  }
-
-  return (
-    <div className="space-x-2">
-      <Button variant={"ghost"}>
-        <Link href="/appwrite/signin">Sign in</Link>
-      </Button>
-      <Button>
-        <Link href="/appwrite/signup">Sign up</Link>
-      </Button>
-    </div>
-  );
+  return <AuthButtonsClient user={user} />;
 }
--- projects/appwrite/layout/Header.tsx
+++ projects/appwrite/layout/Header.tsx
@@ -1,20 +1,18 @@
-"use client";
-
 import Logo from "@/components/Logo";
 import NavDesktop from "./NavDesktop";
 import AuthButtons from "./AuthButtons";
-import { useUser } from "../hooks/useUser";
+import { Suspense } from "react";
 
 export default function Header() {
-  const { user } = useUser();
-
   return (
     <header className="sticky top-0 h-16 z-30 bg-white">
       <div className="container flex items-center justify-between">
         <Logo />
         <div className="flex items-center">
           <NavDesktop />
-          <AuthButtons user={user} />
+          <Suspense fallback={<div>Loading...</div>}>
+            <AuthButtons />
+          </Suspense>
         </div>
       </div>
     </header>
