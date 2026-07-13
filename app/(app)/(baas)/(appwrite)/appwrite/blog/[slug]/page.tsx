65a4b17f5382037b64e52a1bb05393896ce65d6c
mkhotamirais <mkhotamirais@gmail.com>
2026-07-13 07:59:05 +0700

    blog detail

--- app/(app)/(baas)/(appwrite)/appwrite/blog/[slug]/page.tsx
+++ app/(app)/(baas)/(appwrite)/appwrite/blog/[slug]/page.tsx
@@ -1,6 +1,5 @@
 import { getBlogBySlug } from "@/actions/appwrite/blog";
 import Breadcrumb from "@/components/ui/custom/Breadcrumb";
-import { APPWRITE_BUCKET_NEXT_BUCKET, APPWRITE_ENDPOINT } from "@/lib/constants";
 import { diffForHumans } from "@/lib/utils";
 import Image from "next/image";
 
@@ -11,10 +10,6 @@ export default async function BlogDetail({ params }: { params: Promise<{ slug: s
 
   if (!blog) return <div>Blog not found</div>;
 
-  const imageUrl = blog.bannerId
-    ? `${APPWRITE_ENDPOINT}/storage/buckets/${APPWRITE_BUCKET_NEXT_BUCKET}/files/${blog.bannerId}/view?project=${process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID}`
-    : "";
-
   return (
     <section className="max-w-3xl mx-auto my-4">
       <Breadcrumb />
@@ -22,9 +17,9 @@ export default async function BlogDetail({ params }: { params: Promise<{ slug: s
         <h1 className="first-letter:capitalize">{blog.title}</h1>
         <p className="text-xs text-muted-foreground mt-1">{diffForHumans(blog.$createdAt)}</p>
 
-        {imageUrl ? (
+        {blog?.imageId ? (
           <Image
-            src={imageUrl}
+            src={blog.imageUrl}
             alt={blog.title}
             width={500}
             height={300}
